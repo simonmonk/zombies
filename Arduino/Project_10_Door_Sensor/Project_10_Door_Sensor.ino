61ad51d1b10a64f7b8e294a306ccf3c3ee6fed8f
simonmonk <srmonk@gmail.com>
2015-06-03 07:35:10 +0100

    renamed door sensor project

---
+++ Arduino/Project_10_Door_Sensor/Project_10_Door_Sensor.ino
@@ -0,0 +1,73 @@
+
+
+#include <LiquidCrystal.h>
+
+
+// Pin allocations
+const int buzzerPin = 11;
+const int backlightPin = 10;
+const int switchPin = A0;
+const int doorPin = 12;   
+
+//                RS,E,D4,D5,D6,D7         
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+boolean mute = false;
+
+void setup() 
+{
+  // This because of a defect in common cheap LCD displays
+  // backlight controlled by transistor D10 high can
+  // burn out Arduino pin
+  pinMode(backlightPin, INPUT);
+  pinMode(doorPin, INPUT_PULLUP);
+  lcd.begin(16, 2);
+}
+
+void loop() 
+{
+  checkDoor(); 
+  
+  if (analogRead(switchPin) < 1000) // any key pressed
+  {
+    mute = ! mute;
+    if (mute) 
+    {
+      noTone(buzzerPin);
+      lcd.setCursor(12, 1);
+      lcd.print("MUTE");
+    }
+    else
+    {
+      lcd.setCursor(12, 1);
+      lcd.print("    ");
+    }
+    delay(300);
+  }
+  delay(100);
+}
+
+void warn(char message[])
+{
+  lcd.setCursor(0, 1);
+  lcd.print(message);
+  delay(100);
+  lcd.setCursor(0, 1);
+  lcd.print("            ");
+  if (!mute)
+  {
+    tone(buzzerPin, 1000);
+    delay(100);
+    noTone(buzzerPin);
+  }
+  delay(100);
+}
+
+void checkDoor()
+{
+  if (digitalRead(doorPin))
+  {
+    warn("DOOR");
+  }
+}
+
